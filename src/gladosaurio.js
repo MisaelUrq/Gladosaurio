d53060c840577ec37e1f2d9779eb651d93faf642
Misael Urquieta <misaelurquieta198@hotmail.com>
2020-04-01 22:20:00 -0600

    Tenemos un cuadro negro!

--- src/gladosaurio.js
+++ src/gladosaurio.js
@@ -1,11 +1,21 @@
+let canvas = document.getElementById("game_screen");
 
-let last_time = 0;
+if (canvas != null) {
+    let last_time = 0;
+    let ctx = canvas.getContext("2d");
 
-let game_loop = function(current_time) {
-    let dt = current_time - last_time;
-    last_time = current_time;
-    console.log(dt);
-    requestAnimationFrame(game_loop);
+    if (ctx != null) {
+        let game_loop = function(current_time) {
+            let dt = current_time - last_time;
+            last_time = current_time;
+            ctx.fillRect(0, 0, 40, 60);
+            requestAnimationFrame(game_loop);
+        }
+
+        requestAnimationFrame(game_loop);
+    } else {
+        alert("No pudimos obtener el contexto del canvas! D:");
+    }
+} else {
+    alert("El html fue bañado, recargue la página.");
 }
-
-requestAnimationFrame(game_loop);
